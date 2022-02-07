1a70718085a0d3275ef529d9edd7241e4eeb4e81
adimasmudi <adimasmudi@gmail.com>
2022-02-07 20:26:46 +0700

    revision

--- convertword2pdf/convert.py
+++ convertword2pdf/convert.py
@@ -1,6 +1,6 @@
 from docx2pdf import convert
 import os
-path = os.listdir("C:/Users/ACER/AppData/Local/Programs/Python/Python39/Lib/site-packages/docx2pdf/convert")
+path = os.listdir("D:\documents\Python Scripts\convertword2pdf")
 
 # demo for 1 file
 #convert("C:/Users/pi/Desktop/docx2pdf/testfile1.docx",
@@ -8,5 +8,5 @@ path = os.listdir("C:/Users/ACER/AppData/Local/Programs/Python/Python39/Lib/site
 
 for i, files in enumerate(path):
     if files[-5:] == '.docx':
-        convert(f"C:/Users/ACER/AppData/Local/Programs/Python/Python39/Lib/site-packages/docx2pdf/convert/try.docx",
-         f"C:/Users/ACER/AppData/Local/Programs/Python/Python39/Lib/site-packages/docx2pdf/convert/output.pdf")
+        convert(f"D:\documents\Python Scripts\convertword2pdf/try.docx",
+         f"D:\documents\Python Scripts\convertword2pdf/output.pdf")
